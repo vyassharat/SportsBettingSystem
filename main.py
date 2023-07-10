2ed431d79bc1cb6e004e8b6eeaa9cc2d1bac01a8
vyassharat <53922771+vyassharat@users.noreply.github.com>
2023-07-10 19:53:29 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def print_hello_world():
+    # Use a breakpoint in the code line below to debug your script.
+    print('Hello World')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hello_world()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
